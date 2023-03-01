a60a5c6b35525567250352ba3b1a3cc6e8a38264
Ivan <122042934+IvanGrebenyukov@users.noreply.github.com>
2023-03-01 18:20:08 +0500

    Add files via upload

---
+++ Practice11_12_Zadanie4/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace Practice11_12_Zadanie4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(292, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Расчет идеального веса";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(13, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Введите имя";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Gray;
+            this.textBox2.Location = new System.Drawing.Point(13, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Введите фамилию";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.Color.Gray;
+            this.textBox3.Location = new System.Drawing.Point(12, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 23);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Введите отчество";
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 298);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            230,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(159, 23);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            170,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Выберите рост";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(13, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Выберите вес";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(13, 358);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(159, 23);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // textBox5
+            // 
+            this.textBox5.ForeColor = System.Drawing.Color.Gray;
+            this.textBox5.Location = new System.Drawing.Point(12, 399);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(158, 23);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "Введите вид спорта";
+            this.textBox5.Click += new System.EventHandler(this.textBox5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Формула Брока";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 38);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Формула Купера";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(583, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 38);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Формула Моххамеда";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(243, 70);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 38);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Информация о спортсмене";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 239);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(160, 23);
+            this.dateTimePicker1.TabIndex = 16;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Practice11_12_Zadanie4/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Practice11_12_Zadanie4;
+namespace Practice11_12_Zadanie4
+{
+    public partial class Form1 : Form
+    {
+        Athlete sportsmen = new Athlete();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Мужской");
+            comboBox1.Items.Add("Женский");
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Checked();
+        }
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = "";
+            textBox5.ForeColor = Color.Black;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox3.ForeColor = Color.Black;
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox2.ForeColor = Color.Black;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value >= DateTime.Now)
+            {
+                MessageBox.Show("Выберите дату правильно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                sportsmen.Height = Convert.ToDouble(numericUpDown1.Value);
+                MessageBox.Show("Идеальный вес = " + sportsmen.CalculateBrok().ToString(), "Расчет идеального веса по формуле брока", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите пол", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                sportsmen.Gender = comboBox1.SelectedItem.ToString();
+                sportsmen.Height = Convert.ToDouble(numericUpDown1.Value);
+                MessageBox.Show("Идеальный вес = " + sportsmen.CalculateCouper().ToString(), "Расчет идеального веса по формуле Куппера", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                sportsmen.Height = Convert.ToDouble(numericUpDown1.Value);
+                MessageBox.Show("Идеальный вес = " + sportsmen.CalculateMoxxamed().ToString(), "Расчет идеального веса по формуле брока", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+        public void Checked()
+        {
+            string error = "";
+            if (string.IsNullOrWhiteSpace(textBox1.Text) && !Regex.IsMatch(textBox1.Text, "^[а - яА-Я]+$"))
+            {
+                error += "Введите имя правильно";
+            }
+            else
+            {
+                sportsmen.Name = textBox1.Text;
+            }
+            if (string.IsNullOrWhiteSpace(textBox2.Text) && !Regex.IsMatch(textBox1.Text, "^[а - яА-Я]+$"))
+            {
+                error += "\nВведите фамилию правильно";
+            }
+            else
+            {
+                sportsmen.Surname = textBox2.Text;
+            }
+            if (string.IsNullOrWhiteSpace(textBox3.Text) && !Regex.IsMatch(textBox1.Text, "^[а - яА-Я]+$"))
+            {
+                error += "\nВведите отчество правильно";
+            }
+            else
+            {
+                sportsmen.LastName = textBox3.Text;
+            }
+            if (comboBox1.SelectedIndex == -1)
+            {
+                error += "\nВыберите пол";
+            }
+            else
+            {
+                sportsmen.Gender = comboBox1.SelectedItem.ToString();
+            }
+            if (dateTimePicker1.Value >= DateTime.Now)
+            {
+                error += "\nВыберите дату правильно";
+            }
+            else
+            {
+                sportsmen.DateOfBirthday = dateTimePicker1.Value;
+            }
+            if (numericUpDown1.Value <= 0)
+            {
+                error += "\nВыберите рост правильно";
+            }
+            else
+            {
+                sportsmen.Height = Convert.ToDouble(numericUpDown1.Value);
+            }
+            if (numericUpDown2.Value <= 0)
+            {
+                error += "\nВыберите вес правильно";
+            }
+            else
+            {
+                sportsmen.Weight = Convert.ToDouble(numericUpDown2.Value);
+            }
+            if (string.IsNullOrWhiteSpace(textBox5.Text) && !Regex.IsMatch(textBox1.Text, "^[а - яА-Я]+$"))
+            {
+                error += "\nВведите вид спорта правильно";
+            }
+            else
+            {
+                sportsmen.TypeSport = textBox5.Text;
+            }
+            if (error == "")
+            {
+                MessageBox.Show(sportsmen.InfoAthlet(), "Информация о спортсмене", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show(error, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
